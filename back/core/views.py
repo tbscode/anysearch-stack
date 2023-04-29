87095b109d30c653be52cfed595d33f52e2bfc33
tim <tim@timschupp.de>
2023-04-30 01:17:14 +0200

    some redirecting for unauthenticated users

--- back/core/views.py
+++ back/core/views.py
@@ -2,12 +2,14 @@ from django.conf import settings
 import requests
 from django.http import HttpResponse
 from core.api.user_data import get_user_data
+from django.shortcuts import redirect
 
 
 def index(request, path):
     if request.user.is_authenticated:
         return render_nextjs_page(get_user_data(request.user), path=path)
-    return render_nextjs_page({"test": "test"}, path=path)
+    else:
+        return render_nextjs_page({"test": "test"}, path="login")
 
 
 def render_nextjs_page(data, path=""):
